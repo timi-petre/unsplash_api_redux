10379802a75f073c87a1292ae87fd9ff209ded20
Timi Petre <gaxxis@yahoo.com>
2021-12-06 13:03:40 +0200

    Avatar Container code

---
+++ lib/src/container/avatar_container.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+
+import '/src/models/app_state.dart';
+
+class AvatarContainer extends StatelessWidget {
+  const AvatarContainer({Key? key, required this.builder}) : super(key: key);
+
+  final ViewModelBuilder<String> builder;
+
+  @override
+  Widget build(BuildContext context) {
+    return StoreConnector<AppState, String>(
+      converter: (Store<AppState> store) => store.state.avatar,
+      builder: builder,
+    );
+  }
+}
